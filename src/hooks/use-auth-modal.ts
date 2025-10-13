a3e249485f8baa334273db215a42e0b14431f2c8
userduser <ahamed.a.1235@gmail.com>
2025-10-13 22:05:11 +0000

    auto-commit for 6d72235a-4946-4a90-87b9-7684221b37c1

--- src/hooks/use-auth-modal.ts
+++ src/hooks/use-auth-modal.ts
@@ -6,6 +6,7 @@ type AuthModal = 'login' | 'signup' | 'logout' | null;
 
 interface AuthModalStore {
   activeModal: AuthModal;
+  isClosing: boolean;
   openLogin: () => void;
   openSignup: () => void;
   openLogout: () => void;
@@ -14,12 +15,19 @@ interface AuthModalStore {
   switchToSignup: () => void;
 }
 
-export const useAuthModal = create<AuthModalStore>((set) => ({
+export const useAuthModal = create<AuthModalStore>((set, get) => ({
   activeModal: null,
-  openLogin: () => set({ activeModal: 'login' }),
-  openSignup: () => set({ activeModal: 'signup' }),
-  openLogout: () => set({ activeModal: 'logout' }),
-  closeModal: () => set({ activeModal: null }),
-  switchToLogin: () => set({ activeModal: 'login' }),
-  switchToSignup: () => set({ activeModal: 'signup' }),
+  isClosing: false,
+  openLogin: () => set({ activeModal: 'login', isClosing: false }),
+  openSignup: () => set({ activeModal: 'signup', isClosing: false }),
+  openLogout: () => set({ activeModal: 'logout', isClosing: false }),
+  closeModal: () => {
+    set({ isClosing: true });
+    // Delay the actual close to allow animation to complete
+    setTimeout(() => {
+      set({ activeModal: null, isClosing: false });
+    }, 200);
+  },
+  switchToLogin: () => set({ activeModal: 'login', isClosing: false }),
+  switchToSignup: () => set({ activeModal: 'signup', isClosing: false }),
 }));
