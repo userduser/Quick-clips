f67311170e2e9f515e57adfb1bbd2d54b390c205
userduser <ahamed.a.1235@gmail.com>
2025-10-14 06:01:46 +0000

    auto-commit for 21d00b2e-1a16-4801-9b48-5ebfa53cad6c

--- src/components/session-aware-footer.tsx
+++ src/components/session-aware-footer.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { useSession } from "next-auth/react";
 import { Footer } from "~/components/footer";
 
 export function SessionAwareFooter() {
-  const { data: session } = useSession();
-  
-  return <Footer showDashboard={!!session} />;
+  // For now, hide Dashboard link until user logs in
+  // TODO: Implement proper auth check when backend auth is integrated
+  return <Footer showDashboard={false} />;
 }
