b02aa7f41abff9d2009f8048f10e35322f6ae402
userduser <ahamed.a.1235@gmail.com>
2025-10-14 06:01:37 +0000

    auto-commit for 44f68851-b290-4105-b66f-2d9e370e8f11

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,17 +1,12 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import { AuthModalProvider } from "~/components/auth/auth-modal-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider 
-      basePath="/api/auth"
-      refetchInterval={0}
-      refetchOnWindowFocus={false}
-    >
+    <>
       {children}
       <AuthModalProvider />
-    </SessionProvider>
+    </>
   );
 }
