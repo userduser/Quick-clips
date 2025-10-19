abf4f52ece70f34238d33d03899363a55326d3e0
userduser <ahamed.a.1235@gmail.com>
2025-10-19 00:11:12 +0000

    auto-commit for 710cc8f6-08a0-4604-ac8e-47852b557595

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -68,9 +68,11 @@ export function MobileNav() {
           fixed top-16 w-64 h-[calc(100vh-4rem)] 
           bg-black/70
           border-l border-border/40 shadow-2xl z-50 md:hidden
-          transition-all duration-300 ease-in-out
           ${isOpen ? "right-0" : "-right-64"}
         `}
+        style={{
+          transition: 'right 0.3s ease-in-out'
+        }}
       >
         <nav className="flex flex-col p-6 space-y-6">
           <Link 
